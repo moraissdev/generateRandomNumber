d1f24f09ed4a847b892d879f1775cfe74fb07f4a
moraissdev <renanbusinessdev@gmail.com>
2025-04-06 23:27:18 -0300

    commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const buttonToDrawNumber = document.querySelector(".action-button")
 function generateRandomNumber() {
     const entryNumberInputBetween = document.querySelector(".between").value //PEGAR VALOR DIGITADO NO BOTÃO "entre"
     const entryNumberInputAnd = document.querySelector(".and").value // PEGAR VALOR DIGITADO NO BOTÃO "e"
+    
     console.log(entryNumberInputBetween, entryNumberInputAnd)
 }
 
