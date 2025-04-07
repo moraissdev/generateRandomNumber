626679c9441a23a3f8c8d0460c87caeead41b4f2
moraissdev <renanbusinessdev@gmail.com>
2025-04-07 01:47:47 -0300

    commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function generateRandomNumber() {
 
     const resultRandomNumber = Math.floor(Math.random() * (entryMaxNumberInputAnd - entryMinNumberInputBetween) + entryMinNumberInputBetween)
     
-    spaceDisplayDrawNumber.textContent = "→ " + resultRandomNumber
+    spaceDisplayDrawNumber.textContent = "Resultado → " + resultRandomNumber
 }
 buttonToDrawNumber.addEventListener("click", generateRandomNumber)
 //
