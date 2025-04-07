ce7dc9b7ea3594f1713bab263cae82c35e5d4cd0
moraissdev <renanbusinessdev@gmail.com>
2025-04-07 00:57:07 -0300

    commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const buttonToDrawNumber = document.querySelector(".action-button")
-
 function generateRandomNumber() {
     const entryMinNumberInputBetween = Math.ceil(document.querySelector(".between").value) //PEGAR VALOR DIGITADO NO BOTÃO "entre"
     const entryMaxNumberInputAnd = Math.floor(document.querySelector(".and").value) // PEGAR VALOR DIGITADO NO BOTÃO "e"
@@ -10,7 +9,7 @@ function generateRandomNumber() {
     spaceDisplayDrawNumber.textContent = (resultRandomNumber)
 }
 buttonToDrawNumber.addEventListener("click", generateRandomNumber)
-
+//
 const prizeDrawImage = document.querySelector(".image").style.height = "215px"
 //
 const commandName = document.querySelector(".command");
@@ -24,3 +23,9 @@ embeddedContent.style.display = "block"
 embeddedContent.style.textAlign = "center"
 embeddedContent.style.margin = "auto"
 embeddedContent.style.marginTop = "50px"
+//
+const spaceToGenerateNumber = document.querySelector(".number-draw");
+spaceToGenerateNumber.style.display = "block"
+spaceToGenerateNumber.style.margin = "auto"
+spaceToGenerateNumber.style.marginTop = "60px"
+spaceToGenerateNumber.style.textAlign = "center"
