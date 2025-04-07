4cc77cfb3971b761010a67bb7ac2bc4124f281b7
moraissdev <renanbusinessdev@gmail.com>
2025-04-06 23:53:00 -0300

    commit

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const buttonToDrawNumber = document.querySelector(".action-button")
 function generateRandomNumber() {
     const entryMinNumberInputBetween = Math.ceil(document.querySelector(".between").value) //PEGAR VALOR DIGITADO NO BOTÃO "entre"
     const entryMaxNumberInputAnd = Math.floor(document.querySelector(".and").value) // PEGAR VALOR DIGITADO NO BOTÃO "e"
+    const spaceDisplayDrawNumber = document.querySelector(".number-result")
 
     const resultRandomNumber = Math.floor(Math.random() * (entryMaxNumberInputAnd - entryMinNumberInputBetween) + entryMinNumberInputBetween)
     
-    alert(resultRandomNumber)
+    spaceDisplayDrawNumber.textContent = (resultRandomNumber)
 }
 
 
